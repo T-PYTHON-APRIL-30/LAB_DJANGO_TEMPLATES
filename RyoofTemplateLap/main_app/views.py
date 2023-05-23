6feb23ea4326238d99ceef2d0664405ff1baf59a
ryuof alarfaj <ryuofalarfaj@gmail.com>
2023-05-23 14:17:20 +0300

    lap_update

--- RyoofTemplateLap/main_app/views.py
+++ RyoofTemplateLap/main_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from datetime import date
 import random 
+import string
+import secrets
+
 # Create your views here.
 
 
@@ -13,15 +16,23 @@ def today_date(request:HttpRequest):
     return render(request, "main_app/today.html", context)
 
 def random_password(request:HttpRequest):
-    passwords=['gfgf56_ggg','uuyuyu',"YRD97662kjhh&c","dggR344_dd"]
-    context = {"random_password":random.choice(passwords)}
+    letters_upper= string.ascii_uppercase
+    letters_lower=string.ascii_lowercase
+    numbers=string.digits
+    all=letters_lower + letters_upper+ numbers
+    pw_len=8
+    pw = ''
+    
+    for i in range(pw_len):
+        pw += ''.join(secrets.choice(all))
+    context = {"random_password":pw}
 
     return render(request, "main_app/randompassword.html", context)
-
+    # passwords=[random.choices(pw)for i in range(4)]
 
 
 def fav_games(request:HttpRequest):
 
     games = ["Crash", "Call of Duty", "Fortnite"]
-    context = {"favorate_games" : games}
+    context = {"games" : games}
     return render(request, "main_app/favgames.html", context)
