08ed372e72fd8c901fb9ac81a368629dbcb91a85
Dalia <daliaasq@gmail.com>
2023-05-24 10:21:06 +0300

    modify names variable

--- Template_Project/games_app/urls.py
+++ Template_Project/games_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 name = "games_app"
 
 urlpatterns = [
-    path("today/",views.date, name = "games_app"),
-    path("random/password/",views.pass_generater, name = "games_app"),
-    path("favs/games/",views.fav_games,name = "games_app")
+    path("today/",views.date, name = "date"),
+    path("random/password/",views.pass_generater, name = "pass_generater"),
+    path("favs/games/",views.fav_games,name = "fav_games")
 ]
